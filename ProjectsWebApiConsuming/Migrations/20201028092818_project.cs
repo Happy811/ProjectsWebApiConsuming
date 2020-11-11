98bcd5202069ad64e7734ba80c2ac7085735a07e
Harpreet Kaur <HK022@atcltd.atc.org.nz>
2020-11-12 09:50:08 +1300

    Add project files.

---
+++ ProjectsWebApiConsuming/Migrations/20201028092818_project.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectsWebApiConsuming.Migrations
+{
+    public partial class project : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    StartingDate = table.Column<string>(nullable: true),
+                    EndingDate = table.Column<string>(nullable: true),
+                    Budget = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
---
+++ ProjectsWebApiConsuming/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectsWebApiConsuming.Models
+{
+    public class Project // Projects Managing Class
+    {
+        [Key] // PK Project
+        public int Id { get; set; }
+        // title of the project
+        public string Title { get; set; }
+        // starting date
+        public string StartingDate { get; set; }
+        // ending date
+        public string EndingDate { get; set; }
+        // project budget
+        public double Budget { get; set; }
+    }
+}
